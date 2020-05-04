a581594f15e6ce6774c25e92f931fbc1ac134034
RPIUdishan <it18082234@my.sliit.lk>
2020-05-04 16:05:50 +0530

    insert method in PaymentSchemeAPI Servlet

--- src/com/PaymentSchemeAPI.java
+++ src/com/PaymentSchemeAPI.java
@@ -8,12 +8,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
+import beans.PaymentSchemeBean;
+import model.PaymentScheme;
+
 @WebServlet("/PaymentSchemeAPI")
 public class PaymentSchemeAPI extends HttpServlet {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	PaymentScheme ps = new PaymentScheme();
+	PaymentSchemeBean psBean = new PaymentSchemeBean();
+
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		psBean.setDoc_id(Integer.parseInt(req.getParameter("doc_id")));	
+		psBean.setHospital_id(Integer.parseInt(req.getParameter("hospitalID")));
+		psBean.setDoc_charge(Double.parseDouble(req.getParameter("docCharge")));
+		psBean.setHosp_charge(Double.parseDouble(req.getParameter("hospCharge")));
+		psBean.setTax(Double.parseDouble(req.getParameter("tax")));
+		
+		String output = ps.insertPaymentScheme(psBean);
+		
+		resp.getWriter().write(output); 
+		
 	}
 }
