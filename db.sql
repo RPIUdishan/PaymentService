fd4e01bc4085d80c39f8ba0a36429b3ea64d03ae
RPIUdishan <it18082234@my.sliit.lk>
2020-05-06 16:06:11 +0530

    db.sql added - complete

---
+++ db.sql
@@ -0,0 +1,16 @@
+###payment_schemes table
+create table payment_schemes (
+	id int primary key auto_increment, 
+    doc_id int not null,
+    hospital_id int not null,
+    doc_charge double not null,
+    hosp_charge double not null,
+    tax double
+);
+
+
+
+
+
+
+
